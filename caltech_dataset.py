0c69903cc0b662753e846bf9a9edcf3b7fd47283
gallacci97 <58908937+gallacci97@users.noreply.github.com>
2020-11-19 12:10:11 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -7,6 +7,7 @@ import os.path
 import sys
 
 
+
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
@@ -14,13 +15,24 @@ def pil_loader(path):
         return img.convert('RGB')
 
 
+def make_dataset(root, fileName, class_to_idx):
+    fName = "./Caltech101/{0}.txt".format(fileName)
+    dataset = list()
+    with open(fName,'r') as f:
+      for line in f:
+        if line.find('BACKGROUND_Google')==-1:
+            line = line.split('\n')[0]
+            className = line.split('/')[0]
+            dataset.append((pil_loader("{0}/{1}".format(root,line)), class_to_idx[className]))
+    return dataset
+
 class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
         self.split = split # This defines the split you are going to use
                            # (split files are called 'train.txt' and 'test.txt')
-
+        
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
@@ -29,20 +41,40 @@ class Caltech(VisionDataset):
           through the index
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
+        self.root = root
+        self.classes, self.class_to_idx = self._find_classes(self.root)
+        self.samples = make_dataset(self.root, self.split, self.class_to_idx)
+        self.targets = [s[1] for s in self.samples]
+    
+    def _find_classes(self, dir: str):
+        """
+        Finds the class folders in a dataset.
+
+        Args:
+            dir (string): Root directory path.
+
+        Returns:
+            tuple: (classes, class_to_idx) where classes are relative to (dir), and class_to_idx is a dictionary.
+
+        Ensures:
+            No class is a subdirectory of another.
+        """
+        classes = [d.name for d in os.scandir(dir) if d.is_dir() and d.name!='BACKGROUND_Google']
+        classes.sort()
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
 
     def __getitem__(self, index):
-        '''
+        """
         __getitem__ should access an element through its index
         Args:
             index (int): Index
 
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
-        '''
+        """
 
-        image, label = ... # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
+        image, label = self.samples[index]
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
@@ -55,5 +87,5 @@ class Caltech(VisionDataset):
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
+        length = len(self.samples)
         return length
